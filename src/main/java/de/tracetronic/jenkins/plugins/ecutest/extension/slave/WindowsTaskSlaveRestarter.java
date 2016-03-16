5fb366b97c99c9f8ff2624a2118e8625313fb208
Christian Pönisch <christian.poenisch@tracetronic.de>
2016-03-16 08:50:53 +0100

    moved slave restarter extension

--- src/main/java/de/tracetronic/jenkins/plugins/ecutest/extension/slave/WindowsTaskSlaveRestarter.java
+++ src/main/java/de/tracetronic/jenkins/plugins/ecutest/extension/slave/WindowsTaskSlaveRestarter.java
@@ -27,7 +27,7 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package de.tracetronic.jenkins.plugins.ecutest.util.slave;
+package de.tracetronic.jenkins.plugins.ecutest.extension.slave;
 
 import hudson.Extension;
 
