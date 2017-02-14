eb6b9ba6b063b29aa7095ca7d9aa3a324026a6ff
Christian Pönisch <christian.poenisch@tracetronic.de>
2017-02-14 16:42:16 +0100

    updated ATX config.xml to v1.40.0

--- src/main/java/de/tracetronic/jenkins/plugins/ecutest/ETPlugin.java
+++ src/main/java/de/tracetronic/jenkins/plugins/ecutest/ETPlugin.java
@@ -63,7 +63,7 @@ public class ETPlugin extends Plugin {
     /**
      * Defines the TEST-GUIDE version that the provided ATX configuration is based on.
      */
-    public static final ToolVersion ATX_VERSION = new ToolVersion(1, 39, 0, 0);
+    public static final ToolVersion ATX_VERSION = new ToolVersion(1, 40, 0, 0);
 
     /**
      * Helper class to easily compare tool versions defined by major, minor, micro and qualifier version. Mainly used to
