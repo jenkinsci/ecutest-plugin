4ba26d08a37d05eb95593cc3f0aa10d79625e4e1
Christian Pönisch <christian.poenisch@tracetronic.de>
2019-05-22 16:41:10 +0200

    drop support for 6.x and add compatibility up to 8.0

--- src/main/java/de/tracetronic/jenkins/plugins/ecutest/ETPlugin.java
+++ src/main/java/de/tracetronic/jenkins/plugins/ecutest/ETPlugin.java
@@ -37,12 +37,12 @@ public class ETPlugin {
     /**
      * Defines the minimum required ECU-TEST version supported by this plugin.
      */
-    public static final ToolVersion ET_MIN_VERSION = new ToolVersion(6, 3, 0);
+    public static final ToolVersion ET_MIN_VERSION = new ToolVersion(7, 0, 0);
 
     /**
      * Defines the maximum allowed ECU-TEST version supported by this plugin.
      */
-    public static final ToolVersion ET_MAX_VERSION = new ToolVersion(7, 2, 0);
+    public static final ToolVersion ET_MAX_VERSION = new ToolVersion(8, 0, 0);
 
     /**
      * Defines the TEST-GUIDE version that the provided ATX configuration is based on.
