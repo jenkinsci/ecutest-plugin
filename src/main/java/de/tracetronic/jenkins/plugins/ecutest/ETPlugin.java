d4c978b9efd3845f7cfc9a55ce643fc2902413c9
Christian Pönisch <christian.poenisch@tracetronic.de>
2019-05-22 16:50:01 +0200

    update ATX config.xml to v1.67.0

--- src/main/java/de/tracetronic/jenkins/plugins/ecutest/ETPlugin.java
+++ src/main/java/de/tracetronic/jenkins/plugins/ecutest/ETPlugin.java
@@ -47,7 +47,7 @@ public class ETPlugin {
     /**
      * Defines the TEST-GUIDE version that the provided ATX configuration is based on.
      */
-    public static final ToolVersion ATX_VERSION = new ToolVersion(1, 64, 1);
+    public static final ToolVersion ATX_VERSION = new ToolVersion(1, 67, 0);
 
     /**
      * Retains backward compatibility for renamed classes.
