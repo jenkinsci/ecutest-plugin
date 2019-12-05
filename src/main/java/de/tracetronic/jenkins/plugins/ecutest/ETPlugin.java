3bf5a1441c54e0ccf6caece4c3a5b3882ab8fda0
Christian Pönisch <christian.poenisch@tracetronic.de>
2019-12-05 13:09:47 +0100

    Increase compatibility up to ECU-TEST 8.1.0

--- src/main/java/de/tracetronic/jenkins/plugins/ecutest/ETPlugin.java
+++ src/main/java/de/tracetronic/jenkins/plugins/ecutest/ETPlugin.java
@@ -42,7 +42,7 @@ public class ETPlugin {
     /**
      * Defines the maximum allowed ECU-TEST version supported by this plugin.
      */
-    public static final ToolVersion ET_MAX_VERSION = new ToolVersion(8, 0, 0);
+    public static final ToolVersion ET_MAX_VERSION = new ToolVersion(8, 1, 0);
 
     /**
      * Defines the minimum TEST-GUIDE version supported by this plugin.
