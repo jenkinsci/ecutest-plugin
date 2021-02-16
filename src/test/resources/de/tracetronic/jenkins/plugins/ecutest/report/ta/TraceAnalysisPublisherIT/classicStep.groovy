1b04524d16231fa9135bb6fc830cbb640cf8b5b2
Christian Pönisch <christian.poenisch@tracetronic.de>
2021-02-16 17:36:05 +0100

    Fix wrong setting name for TA publisher

--- src/test/resources/de/tracetronic/jenkins/plugins/ecutest/report/ta/TraceAnalysisPublisherIT/classicStep.groovy
+++ src/test/resources/de/tracetronic/jenkins/plugins/ecutest/report/ta/TraceAnalysisPublisherIT/classicStep.groovy
@@ -1,5 +1,10 @@
+/*
+ * Copyright (c) 2015-2021 TraceTronic GmbH
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
 node('windows') {
-    step([$class: 'TraceAnalysisPublisher', toolName: 'ECU-TEST',
-            timeout: '600', mergeReports: true, createReportDirs: true,
-            allowMissing: true, runOnFailed: true])
+    step([$class      : 'TraceAnalysisPublisher', toolName: 'ECU-TEST',
+          timeout     : '600', mergeReports: true, createReportDir: true,
+          allowMissing: true, runOnFailed: true])
 }
