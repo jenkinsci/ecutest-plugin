6e37bce101ecc179a4bc59ffbfa2d7b494a2ab9f
Christian Pönisch <christian.poenisch@tracetronic.de>
2021-03-29 14:11:26 +0200

    Increase compatibility up to ECU-TEST 2021.1.0

--- src/main/java/de/tracetronic/jenkins/plugins/ecutest/ETPlugin.java
+++ src/main/java/de/tracetronic/jenkins/plugins/ecutest/ETPlugin.java
@@ -35,7 +35,7 @@ public class ETPlugin {
     /**
      * Defines the maximum allowed ECU-TEST version supported by this plugin.
      */
-    public static final ToolVersion ET_MAX_VERSION = new ToolVersion(2020, 4, 0);
+    public static final ToolVersion ET_MAX_VERSION = new ToolVersion(2021, 1, 0);
 
     /**
      * Defines the minimum TEST-GUIDE version supported by this plugin.
