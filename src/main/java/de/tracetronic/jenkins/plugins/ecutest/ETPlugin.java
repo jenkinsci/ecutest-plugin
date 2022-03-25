b23446602ad43c14618098e8dc993e04a1f24c8f
Martin Grosche <martin.grosche@tracetronic.de>
2022-03-25 15:29:48 +0100

    Bump up TEST-GUIDE version to 1.120.0 (#403)

--- src/main/java/de/tracetronic/jenkins/plugins/ecutest/ETPlugin.java
+++ src/main/java/de/tracetronic/jenkins/plugins/ecutest/ETPlugin.java
@@ -45,7 +45,7 @@ public class ETPlugin {
     /**
      * Defines the TEST-GUIDE version that the provided ATX configuration is based on.
      */
-    public static final ToolVersion ATX_CONFIG_VERSION = new ToolVersion(1, 113, 0);
+    public static final ToolVersion ATX_CONFIG_VERSION = new ToolVersion(1, 120, 0);
 
     /**
      * Retains backward compatibility for renamed classes.
