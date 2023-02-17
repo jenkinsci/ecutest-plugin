30934dc4abb53eac9969e281015f877209cdf16b
Christoph Wellm <christoph.wellm@tracetronic.de>
2023-02-17 10:58:36 +0100

    Fix code scanning alert trustmanager that accepts all certificates (#523)

--- src/main/java/de/tracetronic/jenkins/plugins/ecutest/report/atx/AbstractATXReportHandler.java
+++ src/main/java/de/tracetronic/jenkins/plugins/ecutest/report/atx/AbstractATXReportHandler.java
@@ -59,9 +59,13 @@ public abstract class AbstractATXReportHandler {
      */
     protected abstract static class AbstractReportCallable<T> extends MasterToSlaveCallable<T, IOException> {
 
+
         private static final long serialVersionUID = 1L;
 
-        private final ATXConfig config;
+        /**
+         * ATX config.
+         */
+        protected final ATXConfig config;
         private final List<FilePath> reportFiles;
         private final EnvVars envVars;
         private final TaskListener listener;
